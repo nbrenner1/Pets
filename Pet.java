d296947190ebd045e049bd54196f491453c24ac1
Nick Brenner <144737153+nbrenner1@users.noreply.github.com>
2023-11-10 18:29:09 -0500

    Create Pet.java

---
+++ Pet.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+interface Pet
+{
+    public void play();
+
+    public void feed();
+
+    public void makeSound();
+
+    public Happiness getHappiness();
+
+    public Hunger getHunger();
+
+    public Color getColor();
+}
